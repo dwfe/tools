54e8a4f8e034865e2bdd8c240d7240a89e1e86b1
z <alex@dwfe.ru>
2021-04-17 04:31:45 +0300

    .

--- packages/rxjs/src/re-export/index.ts
+++ packages/rxjs/src/re-export/index.ts
@@ -1,5 +1,5 @@
 import {animationFrame, asap, async, BehaviorSubject, combineLatest, fromEvent, merge, Observable, queue, Subject, Subscription} from 'rxjs'
-import {debounceTime, delay, distinctUntilChanged, filter, first, map, mapTo, mergeMap, shareReplay, skip, switchMap, takeUntil, tap, throttleTime} from 'rxjs/operators'
+import {debounceTime, delay, distinctUntilChanged, filter, first, map, mapTo, mergeMap, shareReplay, skip, switchMap, takeUntil, tap, throttleTime, withLatestFrom} from 'rxjs/operators'
 
 export {
   Observable,
@@ -24,6 +24,7 @@ export {
   throttleTime,
   shareReplay,
   takeUntil,
+  withLatestFrom,
 
   async,          // Schedules on the macro task queue
   asap,           // Schedules on the micro task queue
