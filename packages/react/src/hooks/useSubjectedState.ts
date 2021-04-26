113e8d3d0128c27963e6d35ecc4753a576132e8d
z <alex@dwfe.ru>
2021-04-27 00:00:32 +0300

    .

--- packages/react/src/hooks/useSubjectedState.ts
+++ packages/react/src/hooks/useSubjectedState.ts
@@ -1,13 +1,19 @@
-import {BehaviourSubj} from '@do-while-for-each/rxjs'
-import {useCallback, useState} from 'react'
+import {useCallback, useEffect, useState} from 'react'
+import {Subj} from '@do-while-for-each/rxjs'
 import {useControlledRender} from './useControlledRender'
 
-export const useSubjectedState = <T = any>(initValue: T): [BehaviourSubj<T>, (value: T) => void] => {
-  const [wrap] = useState(new BehaviourSubj<T>(initValue))
+export const useSubjectedState = <T = any>(initValue: T): [Subj<T>, (value: T) => void] => {
+  const [wrap] = useState<Subj>(new Subj<T>({type: 'shareReplay', bufferSize: 1}, initValue))
   const renderRunFn = useControlledRender()
+
   const setValue = useCallback((value: T) => {
     wrap.setValue(value)
     renderRunFn()
   }, [wrap, renderRunFn])
+
+  useEffect(() => {
+    wrap.stop()
+  }, [])
+
   return [wrap, setValue]
 }
