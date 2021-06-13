ed72a3b35c3cc51b16bf8244f48547ffa9aeee59
z <alex@dwfe.ru>
2021-06-14 00:13:50 +0300

    .

--- packages/common/src/math.ts
+++ packages/common/src/math.ts
@@ -1,3 +1,7 @@
 export const isNumeric = data =>
   !isNaN(data) && !isNaN(parseFloat(data)) && isFinite(data)
 ;
+
+export const divideWithoutRemainder = (value, by) =>
+  (value - value % by) / by
+;
