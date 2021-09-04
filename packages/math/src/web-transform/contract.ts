ac076eeabbeaa05eb0fff706b4a851eb02c19ada
z <alex@dwfe.ru>
2021-09-05 00:23:02 +0300

    .

--- packages/math/src/web-transform/contract.ts
+++ packages/math/src/web-transform/contract.ts
@@ -1,9 +1,9 @@
 /*
  * Homogeneous coordinates on RP**2
  * https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/matrix
- *                    a c e
- * [a,b,c,d,e,f]  =>  b d f , here: linear part - a b c d
- *                    0 0 1    translation part - e f
+ *                    a c e   here is described:
+ * [a,b,c,d,e,f]  =>  b d f ,   a b c d - linear transformation
+ *                    0 0 1     e f     - translation to apply
  */
 export type TWebMatrix = [number, number, number, number, number, number];
 
